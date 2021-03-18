c8ce4e475b338a6c051b0b94b74922da8dd2b87b
David Steel <webdevsteel@gmail.com>
2021-03-18 13:49:05 +1300

    build controls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import classes from './BuildControls.module'
+
+export const BuildControls = (props) => {
+    return (
+        <div className={classes.BuildControls}>
+            
+        </div>
+    )
+}
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React from "react";
 
+import classes from "./Burger.module.css";
+import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
 
-import classes from './Burger.module.css'
-import BurgerIngredients from './BurgerIngredients/BurgerIngredients'
+export const Burger = ({ ingredients }) => {
+  let transformedIngredients = Object.keys(ingredients).map((ingredient) => {
+    return [...Array(ingredients[ingredient])].map((_, i) => {
+      return <BurgerIngredients key={ingredient + i} type={ingredient} />;
+    });
+  }).reduce((arr, el) => {
+      return arr.concat(el)
+  }, []);
 
-export const Burger = ({ingredients}) => {
-    const transformedIngredients = Object.keys(ingredients).map(ingredient => {
-        return [...Array(ingredients[ingredient])].map((_, i) => {
-           return <BurgerIngredients key={ingredient + i} type={ingredient} />
-        })
-    })
-    
-    console.log(transformedIngredients)
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
 
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngredients type="bread-top"/>
-            {transformedIngredients}
-            <BurgerIngredients type="bread-bottom"/>
-        </div>
-    )
-}
+  console.log(transformedIngredients);
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredients type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredients type="bread-bottom" />
+    </div>
+  );
+};
