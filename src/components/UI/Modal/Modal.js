09b7bf6f0e050d4efc2bd1834a4e89a75706c573
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-04-09 11:37:52 +1200

    cleaning up folder structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,17 +4,6 @@ import Backdrop from '../Backdrop/Backdrop'
 import classes from './Modal.module.css'
 
 class Modal extends React.Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log(nextProps)
-    console.log(this.props.show)
-    console.log(nextState)
-    return nextProps.show !== this.props.show
-  }
-
-  componentDidUpdate() {
-    console.log('[Modal] Will Update')
-  }
-
   render() {
     return (
       <>
@@ -23,7 +12,7 @@ class Modal extends React.Component {
           className={classes.Modal}
           style={{
             transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: this.props.show ? '1' : '0',
+            opacity: this.props.show ? '1' : '0'
           }}
         >
           {this.props.children}
